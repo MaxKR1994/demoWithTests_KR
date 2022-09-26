79e540ab054dae85fbb4e0fb9eb29c6e7331d718
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-26 13:37:34 +0300

    refactoring

--- src/main/java/com/example/demowithtests/util/config/orika/EmployeeConverter.java
+++ src/main/java/com/example/demowithtests/util/config/orika/EmployeeConverter.java
@@ -1,4 +1,4 @@
-package com.example.demowithtests.util.config;
+package com.example.demowithtests.util.config.orika;
 
 import com.example.demowithtests.domain.Employee;
 import com.example.demowithtests.dto.createDto.EmployeeAdminDto;
