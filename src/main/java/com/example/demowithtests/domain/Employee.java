a1af926b435e7495f1e5dcf94829444f194c3fa8
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-11-02 18:11:56 +0200

    added getters and setters for new fields

--- src/main/java/com/example/demowithtests/domain/Employee.java
+++ src/main/java/com/example/demowithtests/domain/Employee.java
@@ -117,4 +117,20 @@ public class Employee {
     public void setGender(Gender gender) {
         this.gender = gender;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
